af036b15879f9d120f0d2ba66ac8526dc1cd2256
astable <astable@Santis-Mac-mini.local>
2021-02-20 18:31:08 +0100

    Fecha de creación para producto

--- src/main/java/com/sanvalero/myshop/domain/Product.java
+++ src/main/java/com/sanvalero/myshop/domain/Product.java
@@ -1,7 +1,10 @@
 package com.sanvalero.myshop.domain;
 
+import java.time.LocalDate;
+
 public class Product {
 
     private long id;
     private String name;
+    private LocalDate creationDate;
 }
